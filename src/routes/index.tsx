e0b1c4061d79485cfe64f6c0b12a3dcc25f11a2a
Jean Martins <jeanmrtns4@gmail.com>
2023-07-17 17:08:30 -0300

    fix: change router structure

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,22 @@
-import { createBrowserRouter } from "react-router-dom"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"
 import Root from "./root"
 import { CategoryList } from "../features/categories/CategoryList"
 import { EditCategory } from "../features/categories/EditCategory"
 import { CreateCategory } from "../features/categories/CreateCategory"
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <h1>Page not found</h1>,
-    children: [
-      {
-        index: true,
-        element: <CategoryList />,
-      },
-      {
-        path: "categories",
-        element: <CategoryList />,
-      },
-      {
-        path: "categories",
-        children: [
-          {
-            path: "edit/:id",
-            element: <EditCategory />,
-          },
-          {
-            path: "create",
-            element: <CreateCategory />,
-          },
-        ],
-      },
-    ],
-  },
-])
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route path="/" element={<CategoryList />} />
+      <Route path="/categories">
+        <Route index element={<CategoryList />} />
+        <Route path="/categories/create" element={<CreateCategory />} />
+        <Route path="/categories/edit/:id" element={<EditCategory />} />
+      </Route>
+    </Route>,
+  ),
+)
